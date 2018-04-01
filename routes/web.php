fc765ffad9d8aced57ca4254f2cd7254948d3893
giorgi janjulia <giorgi.janjulia@gtu.ge>
2018-04-01 18:55:05 +0400

    Complex oparator for buyer

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,5 @@ use App\Category;
 */
 
 Route::get('/', function () {
-    // $seller = Seller::has('products')->get()->random(); // seller aris user romesac mibmuli aqvs produqti
-    // $buyer  = User::all()->except($seller->id);        // buyer 
-
-    // factory(Product::class, $productQuantity)->create()->each(function($product) {
-    //     $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
-    //     // attach is shemtxvevashi masivi miebmeva masivs tavisi id ebidan gamomdinare
-    //     $product->categories()->attach($categories);
-    // });
-
-    // $product = Product::with('categories')->get()->each(function($prod) {
-    //     $cat = Category::all()->random(mt_rand(1, 5))->pluck('id');
-    //     $prod->categories()->attach($cat);
-    // });
-
-    $product = Product::with('categories')->first();
-    $product->categories()->attach(['1', '2', '3']);
-    return $product;
-
-    // 11 end next 12
-
+    
 });
