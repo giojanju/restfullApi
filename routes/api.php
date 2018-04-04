623c764be2d7ee1535617fc200a980720187b01e
Giorgi Janjulia <janju@circle.ge>
2018-04-04 12:35:14 +0400

    Product-category

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,9 @@ Route::resource('categories.buyers', 'Category\CategoryBuyerController', ['only'
  * Products
  */
 Route::resource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions', 'Product\ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyer', 'Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.categories', 'Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 
 /**
  * transactions
