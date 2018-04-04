dfca76b86aa582a7afb8cc267042edfc904b3357
Giorgi Janjulia <janju@circle.ge>
2018-04-04 17:30:14 +0400

    Products and buyer

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Product;
 
 use App\Product;
+use App\Category;
 use Illuminate\Http\Request;
 use App\Http\Controllers\ApiController;
 
@@ -18,5 +19,23 @@ class ProductCategoryController extends ApiController
         $categories = $product->categories;
 
         return $this->showAll($categories);
+    }
+
+    public function update(Request $request, Product $product, Category $category)
+    {
+        // attach, sync, syncWithoutDetach
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
+    }
+
+    public function destroy(Product $product, Category $category)
+    {
+        if (!$product->categories()->find($category->id)) {
+            return $this->errorResponse('The specify category is not a category of this product', 404);
+        }
+        $product->categories()->detach($category->id);
+
+        return $this->showAll($product->categories);
     }
 }
